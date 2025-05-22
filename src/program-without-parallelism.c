c0dc857df5fb9174f6cd81d630a8ca634549848c
vitoralvesp <pereira.alves.vitor@outlook.com>
2025-05-22 17:38:28 -0300

    adicionando a documentação do programa não paralelizado

--- src/program-without-parallelism.c
+++ src/program-without-parallelism.c
@@ -1,3 +1,9 @@
+/**
+ * @brief Number of Submatrices that Sum to K
+ * @details Problem A, 23, Solução sem Paralelismo.
+ * @authors Eduardo Takashi Missaka (10417877), Tiago Silveira Lopes (10417600), Vitor Alves Pereira (10410862).
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,11 +11,18 @@
 #define MAX_M 1000
 #define MAX_N 1000
 
-int A[1000][1000];     
-int ps[1010][1010];    
+int A[1000][1000];
 
 typedef long long ll;
 
+/**
+ * @brief Realiza a contagem dos elementos dos subarrays que possuem soma equivalente a k
+ * @details Complexidade de Tempo de O(n^2)
+ * @param nums subarray
+ * @param size tamanho do subarray
+ * @param k valor k para comparacao
+ * @return total de 1 subarray com soma equivalente a k
+ */
 ll count_subarrays_with_sum_k(int *nums, int size, int k) {
     ll count = 0;
     for (int start = 0; start < size; start++) {
@@ -23,12 +36,20 @@ ll count_subarrays_with_sum_k(int *nums, int size, int k) {
     return count;
 }
 
+/**
+ * @brief Calcula o total de subarrays com soma equivalente a k
+ * @details Complexidade de Tempo de O(n^3)
+ * @param m dimensao m da matriz
+ * @param n dimensao n da matriz
+ * @param k valor k para comparacao
+ * @return total de subarrays com soma equivalente a k
+ */
 ll solve(int m, int n, int k) {
     ll ans = 0;
     int temp[MAX_N];
 
     for (int top = 0; top < m; top++) {
-        memset(temp, 0, sizeof(temp)); 
+        memset(temp, 0, sizeof(temp)); // temp[0..n] = temp[0,0,0,...,0]
 
         for (int bottom = top; bottom < m; bottom++) {
             for (int j = 0; j < n; j++)
@@ -42,13 +63,18 @@ ll solve(int m, int n, int k) {
 }
 
 int main() {
-    int k, m, n;
+
+    int k, m, n; // Valor de k e Dimensoes da Matriz
+    ll solution;
+
     scanf("%d %d %d", &k, &m, &n);
 
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
             scanf("%d", &A[i][j]);
 
-    printf("%lld\n", solve(m, n, k));
+    solution = solve(m, n, k);
+    printf("%lld\n", solution);
+    
     return 0;
 }
